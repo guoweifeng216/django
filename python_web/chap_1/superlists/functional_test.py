9a7ddde30b032d231e9e18725403f6bbcb0c74cf
weifeng.guo <weifeng.guo@cnexlabs.com>
2018-10-30 15:54:53 +0800

    superlists/functional_test.py:Changed

--- python_web/chap_1/superlists/functional_test.py
+++ python_web/chap_1/superlists/functional_test.py
@@ -5,8 +5,22 @@
 @filename:functional_test
 """
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        # self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)  # ➎
+        self.fail('Finish the test!')  # ➏
+
+
+if __name__ == '__main__': #➐
+    unittest.main(warnings='ignore') #➑
+
